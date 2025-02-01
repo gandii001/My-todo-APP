90ed9ac4b80273762bf4f5d585ee21afbc52edc4
gandii001 <surajudeen2003@gmail.com>
2025-02-01 01:51:22 +0000

    new commit

--- myfunctions.py
+++ myfunctions.py
@@ -2,7 +2,7 @@
 # doc strings are used for commenting and explaining internally what a function is for
 
 
-def get_todos(filepath="/home/gandii/PycharmProjects/pythonProject310/mytodos.txt"):
+def get_todos(filepath="//home/gandii/PycharmProjects/pythonProject310/mytodos.txt"):
     # example of a doc string
     # """ This function is for reading out the content of a file """
 
